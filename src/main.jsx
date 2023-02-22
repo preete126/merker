9ba35c4430d6c6f37fa1ff3929ba12f363bba04e
Toluene1 <adaramoti111@gmail.com>
2023-02-23 00:38:09 +0100

    added the verifyOTP page and directry on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,21 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import App from "./App";
 import GlobalProvider from "./Provider/GlobalProvider";
-import "bootstrap/dist/js/bootstrap.bundle.min"
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Suspense } from "react";
-import './assets/styles/index.scss'
+import "./assets/styles/index.scss";
 
-const Signup = React.lazy(()=> import("./pages/Signup"))
-const Login = React.lazy(()=> import("./pages/Login"))
-const ResetPassword = React.lazy(()=> import("./pages/forgotPassword/ResetPassword"))
-const Otp = React.lazy(()=> import("./pages/forgotPassword/otp"))
-const New_code = React.lazy(()=> import("./pages/forgotPassword/new_password"))
-const Update = React.lazy(()=> import("./pages/forgotPassword/update"))
-
+const Signup = React.lazy(() => import("./pages/Signup"));
+const Login = React.lazy(() => import("./pages/Login"));
+const ResetPassword = React.lazy(() =>
+  import("./pages/forgotPassword/ResetPassword")
+);
+const Otp = React.lazy(() => import("./pages/forgotPassword/otp"));
+const New_code = React.lazy(() =>
+  import("./pages/forgotPassword/new_password")
+);
+const Update = React.lazy(() => import("./pages/forgotPassword/update"));
+const VerifyOTP = React.lazy(() => import("./pages/verifyOTP"));
 
 const router = createBrowserRouter([
   {
@@ -30,20 +34,24 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
+    path: "/verify",
+    element: <VerifyOTP />,
+  },
+  {
     path: "/reset",
     element: <ResetPassword />,
   },
   {
     path: "/otp",
-    element: <Otp/>,
+    element: <Otp />,
   },
   {
     path: "/new_code",
-    element: <New_code/>,
+    element: <New_code />,
   },
   {
     path: "/update",
-    element: <Update/>,
+    element: <Update />,
   },
 ]);
 
---
+++ src/pages/verifyOTP.jsx
@@ -0,0 +1,79 @@
+import { useContext } from "react";
+import logo from "../../assets/images/hacks-removebg-preview.png";
+import otp_icon from "../../assets/images/otp-icon.png";
+import Alert from "../components/Alert";
+// import { GlobalContext } from "../../Provider/GlobalProvider";
+import "../../assets/styles/Auth.css";
+import { useNavigate } from "react-router-dom";
+
+const VerifyOTP = () => {
+  //   const { email } = useContext(GlobalContext);
+  const navigate = useNavigate();
+  const [alert, setAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const closeAlert = () => {
+    setAlert(false);
+  };
+
+  return (
+    <>
+      <section
+        className="bg-light px-2 px-sm-0 w-100 d-flex align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <main
+          className="m-auto text-center otp_container"
+          style={{ width: "360px", fontFamily: "'Poppins', sans-serif" }}
+        >
+          <div>
+            <img src={logo} width={250} alt="" />
+          </div>
+          <div className="fs-3 mt-4">Two-Step Verification</div>
+          <div className="my-4">
+            <img src={otp_icon} alt="" />
+          </div>
+          <div className="text-dark" style={{ fontSize: "17px" }}>
+            Enter the verification code we sent to
+          </div>
+          <div className="fs-5 py-1">{email}</div>
+          <form action="" onSubmit={newCode}>
+            <div
+              className="otpForm d-flex mx-auto my-3"
+              style={{ maxWidth: "fit-content" }}
+            >
+              <input
+                className="rounded-start"
+                type="text"
+                onKeyUp={next}
+                required
+                maxLength={1}
+              />
+              <input type="text" required maxLength={1} onKeyUp={next} />
+              <input type="text" required maxLength={1} onKeyUp={next} />
+              <input
+                className="rounded-end"
+                required
+                type="text"
+                onKeyUp={next}
+                maxLength={1}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btnForm my-4 p-3 w-100 rounded d-block fs-5"
+            >
+              Verify
+            </button>
+            <div className="policy text-dark">
+              Didn't get the code? <a href="">Resend it</a>
+            </div>
+          </form>
+        </main>
+      </section>
+    </>
+  );
+};
+
+export default Otp;
